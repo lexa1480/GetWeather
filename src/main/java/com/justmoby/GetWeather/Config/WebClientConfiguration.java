73c2aa4ca5fe61c197249334f115c084aa505190
lexa1480 <lexa.f14@gmail.com>
2025-02-10 00:28:37 +0300

    Fix

--- src/main/java/com/justmoby/GetWeather/Config/WebClientConfiguration.java
+++ src/main/java/com/justmoby/GetWeather/Config/WebClientConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -23,19 +22,6 @@ public class WebClientConfiguration
     @Bean
     public WebClient webClientWithTimeout()
     {
-        /*final var tcpClient = TcpClient
-                .create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
-                .doOnConnected(connection -> {
-                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-                });
-
-        return WebClient.builder()
-                .baseUrl(BASE_URL)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-                .build();*/
-
         ExchangeStrategies strategies = ExchangeStrategies.builder()
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(256 * 1024))
                 .build();
