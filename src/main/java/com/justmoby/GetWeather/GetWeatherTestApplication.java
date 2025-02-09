712f0c51f49e5304135ebf0b47a8dcc1a181263e
lexa1480 <lexa.f14@gmail.com>
2025-02-09 18:13:35 +0300

    Initial commit

---
+++ src/main/java/com/justmoby/GetWeather/GetWeatherTestApplication.java
@@ -0,0 +1,13 @@
+package com.justmoby.GetWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetWeatherTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetWeatherTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justmoby/GetWeather/GetWeatherTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justmoby.GetWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetWeatherTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
