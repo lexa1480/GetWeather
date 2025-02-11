11eba7bf2f26d9712451636b3f9c81bd0b007988
lexa1480 <lexa.f14@gmail.com>
2025-02-11 23:13:26 +0300

    ExceptionController - update

--- src/main/java/com/justmoby/GetWeather/Controller/ExceptionController.java
+++ src/main/java/com/justmoby/GetWeather/Controller/ExceptionController.java
@@ -1,9 +1,8 @@
 package com.justmoby.GetWeather.Controller;
 
-import com.justmoby.GetWeather.Utils.CityNotFoundException;
-import com.justmoby.GetWeather.Utils.NetworkException;
-import com.justmoby.GetWeather.Utils.WeatherErrorResponse;
-import com.justmoby.GetWeather.Utils.WeatherNotFoundException;
+import com.justmoby.GetWeather.Utils.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +11,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionController
 {
+    public static final Logger LOG = LoggerFactory.getLogger(ExceptionController.class);
+
+
     @ExceptionHandler(value = CityNotFoundException.class)
     private ResponseEntity<WeatherErrorResponse> handleException(CityNotFoundException cityNotFoundException)
     {
@@ -28,10 +30,18 @@ public class ExceptionController
         return new ResponseEntity<>(weatherErrorResponse, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(value = NetworkException.class)
-    private ResponseEntity<WeatherErrorResponse> handleException(NetworkException networkException)
+    @ExceptionHandler(value = HttpClientServerErrorException.class)
+    private ResponseEntity<WeatherErrorResponse> handleException(HttpClientServerErrorException httpClientServerErrorException)
     {
-        WeatherErrorResponse weatherErrorResponse = new WeatherErrorResponse(networkException.getMessage(), System.currentTimeMillis());
+        WeatherErrorResponse weatherErrorResponse = new WeatherErrorResponse(httpClientServerErrorException.getMessage(), System.currentTimeMillis());
+
+        return new ResponseEntity<>(weatherErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler()
+    private ResponseEntity<WeatherErrorResponse> handleException(Exception exception)
+    {
+        WeatherErrorResponse weatherErrorResponse = new WeatherErrorResponse(exception.getMessage(), System.currentTimeMillis());
 
         return new ResponseEntity<>(weatherErrorResponse, HttpStatus.BAD_REQUEST);
     }
